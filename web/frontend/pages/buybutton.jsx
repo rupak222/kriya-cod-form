f82d0010e3d8e91d6403f48452673afcb1a293bd
rupak-barman <rupakbarman19@gmail.com>
2023-03-21 12:37:32 +0530

    attempt 13

--- web/frontend/pages/buybutton.jsx
+++ web/frontend/pages/buybutton.jsx
@@ -125,7 +125,7 @@ export default function BuyButton() {
   );
 
   const activator = (
-    <Button onClick={togglePopoverActive} >
+    <Button onClick={togglePopoverActive} disclosure>
       Shadow Color
     </Button>
   );
@@ -384,7 +384,7 @@ export default function BuyButton() {
                     min={0}
                     max={10}
                   />
-                  <Popover
+                 {/* <Popover
                     active={popoverActive}
                     activator={activator}
                     // autofocusTarget="first-node" 
@@ -457,7 +457,7 @@ export default function BuyButton() {
                         }}
                       />
                     </div>
-                  </Popover>
+                  </Popover>*/}
                 </Stack>
               </Stack>
 
